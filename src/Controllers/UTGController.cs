c837cd65814d107432b7febc13f3f564301cfc53
bkn6672 <bhushan.khajone@springernature.com>
2018-06-21 11:08:27 +0530

    First Commit

---
+++ src/Controllers/UTGController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UTG_Design.Controllers
+{
+    public class UTGController : Controller
+    {
+        // GET: UTG
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UTG_Design.MvcApplication" Language="C#" %>
